935676899fa8ebf31056103e397f83fc71d1e6e3
Carolyn <carolyn.chan.01@gmail.com>
2019-03-20 23:46:53 -0700

    updata

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject player;
+    Animator a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+      if (collision.collider.name == player.name)
+        {
+            Debug.Log("Check");
+            a.Play("box_top");
+
+
+        }
+    }
+}
