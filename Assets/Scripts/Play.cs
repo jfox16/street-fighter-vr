e2a5b1f2b21f5b99834980ee222492733601d79c
Carolyn <carolyn.chan.01@gmail.com>
2019-03-22 10:34:23 -0700

    updata

--- Assets/Scripts/Play.cs
+++ Assets/Scripts/Play.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Play : MonoBehaviour
 {
-   
+    public GameObject player;
+    Animator a;
+     void Start()
+    {
+        a = GetComponent<Animator>();
+    }
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.Log(hit.gameObject);
+    }
+
 
 }
