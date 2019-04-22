fcc611955cfa4b01b00b6a551ad389474f6ef11b
Carolyn <carolyn.chan.01@gmail.com>
2019-04-21 19:43:05 -0700

    Revert "Revert "updata""
    This reverts commit 69f4533afd9f229512e40fa15722d4e43d771e84.

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+
+    Animator a;
+    private int check;
+   // public GameObject punchBag;
+    void Start()
+    {
+        a = GetComponent<Animator>();
+        check = 0;
+    }
+    IEnumerator OnTriggerEnter(Collider other)
+    { 
+        a.Play("box_open", 0, .25f);
+        yield return new WaitForSeconds(1.5f);
+        if (check == 0)
+        {
+           // GameObject newPB = Instantiate(punchBag);
+            check = 1;
+        }
+       
+
+
+    }
+
+    
+}
