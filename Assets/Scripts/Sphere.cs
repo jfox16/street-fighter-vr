6d18f68ae83358a3711a5a5f98a40c57453fc1ea
Carolyn <carolyn.chan.01@gmail.com>
2019-04-26 01:50:42 -0700

    updata

--- Assets/Scripts/Sphere.cs
+++ Assets/Scripts/Sphere.cs
@@ -2,21 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Minimap : MonoBehaviour
+public class Sphere : MonoBehaviour
 {
     public Fighter fighter;
-
     // Start is called before the first frame update
     void Start()
     {
-        transform.Rotate(90, 0, 0);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(fighter.transform.position.x, fighter.transform.position.y + 50, fighter.transform.position.z);
-       
+        gameObject.transform.position = new Vector3(fighter.transform.position.x, fighter.transform.position.y, fighter.transform.position.z);
     }
     public void setFighter(Fighter f)
     {
