ea46c1fb2673d05985162dc758afdbb7d0d3acf9
Alex Ha <aha2@mail.sfsu.edu>
2019-04-30 22:54:34 -0700

    starting unity-chan

--- Assets/Scripts/Fighter/UnityChanInput.cs
+++ Assets/Scripts/Fighter/UnityChanInput.cs
@@ -5,15 +5,6 @@ using UnityEngine;
 public class UnityChanInput : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] float health = 100;
-
-    [SerializeField] GameObject punchPrefab;
-    [SerializeField] GameObject kickPrefab;
-
-    Animator animator;
-    FPLook fpLook;
-    Transform punchPointTransform;
-    Transform kickPointTransform;
     void Start()
     {
         
@@ -22,12 +13,5 @@ public class UnityChanInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-       
-        if (Input.GetButtonDown("Punch") || Input.GetMouseButtonDown(0))
-        {
-            Instantiate(punchPrefab, punchPointTransform);
-            animator.SetTrigger("Jab");
-        }
     }
 }
