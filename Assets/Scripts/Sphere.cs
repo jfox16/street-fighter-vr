a4d8e861bbc152b57ef26c413142ed642eaa6627
Alex Ha <aha2@mail.sfsu.edu>
2019-04-30 22:40:48 -0700

    added folders for scripts tried to delete animatii

--- Assets/Scripts/Sphere.cs
+++ Assets/Scripts/Sphere.cs
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-﻿using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Sphere : MonoBehaviour
@@ -23,29 +21,3 @@ public class Sphere : MonoBehaviour
     }
 
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Sphere : MonoBehaviour
-{
-    public Fighter fighter;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        gameObject.transform.position = new Vector3(fighter.transform.position.x, fighter.transform.position.y, fighter.transform.position.z);
-    }
-    public void setFighter(Fighter f)
-    {
-        fighter = f;
-    }
-
-}
->>>>>>> 688f6b1587d8b5c1a065668bd5794e7a4848651e
