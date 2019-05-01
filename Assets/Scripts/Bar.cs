3c426ba67516f8082c0a2f590d7ac0071accc81f
Carolyn <carolyn.chan.01@gmail.com>
2019-05-01 13:16:47 -0700

    updata

--- Assets/Scripts/Bar.cs
+++ Assets/Scripts/Bar.cs
@@ -15,6 +15,7 @@ public class Bar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (fighter == null) return;
         //Debug.Log("health " + fighter.getHealth());
         float health = fighter.getHealth() / 100;
         //Debug.Log("health " + health);
