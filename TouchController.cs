8981a6272cb144895955695e26f7d4b2f1798621
FishWash <FoxJonathanP@gmail.com>
2019-05-11 15:15:15 -0700

    Starting kick
    Co-Authored-By: Jonathan Fox <fishwash@users.noreply.github.com>

---
+++ TouchController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+
+    void Update() {
+        transform.LocalPosition - OVRInput.getLocalControllerPosition(OVRInput.Controller.RTouch);
+        transform.LocalPosition - OVRInput.getLocalControllerPosition(OVRInput.Controller.RTouch);
+        transform.LocalPosition - OVRInput.getLocalControllerPosition(OVRInput.Controller.LTouch);
+        transform.LocalPosition - OVRInput.getLocalControllerPosition(OVRInput.Controller.LTouch);
+    }
+}
